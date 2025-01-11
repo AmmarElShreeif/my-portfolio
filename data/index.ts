d2d5f8071cc380c100544b1714eb574185a93876
Ammar <ammar.elshreeif@gmail.com>
2025-01-11 22:37:57 +0200

    secound commit

--- data/index.ts
+++ data/index.ts
@@ -1,118 +1,117 @@
 export const navItems = [
- {
-  name: "Home",
-  link: "#",
- },
- { name: "Experience", link: "#experience" },
- { name: "Proejcts", link: "#projects" },
- { name: "Contact", link: "#contact" },
+  {
+    name: "Home",
+    link: "#",
+  },
+  { name: "Experience", link: "#experience" },
+  { name: "Proejcts", link: "#projects" },
+  { name: "Contact", link: "#contact" },
 ];
-
 
 export const projects = [
- {
-  id: 1,
-  title: "A Restaurant Website",
-  des: "On this site, I review my experience in React Js, React Router, Redux, and file organization",
-  img: "/projects/Bistro.jpg",
-  iconLists: ["/re.svg", "/tail.svg", "/redux.svg"],
-  link: "/ui.earth.com",
- },
- {
-  id: 2,
-  title: "Yoom - Video Conferencing App",
-  des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-  img: "/p2.svg",
-  iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-  link: "/ui.yoom.com",
- },
- {
-  id: 3,
-  title: "AI Image SaaS - Canva Application",
-  des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-  img: "/p3.svg",
-  iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-  link: "/ui.aiimg.com",
- },
- {
-  id: 4,
-  title: "Animated Apple Iphone 3D Website",
-  des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-  img: "/p4.svg",
-  iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-  link: "/ui.apple.com",
- },
+  {
+    id: 1,
+    title: "A Restaurant Website",
+    des: "On this site, I review my experience in React Js, React Router, Redux, and file organization",
+    img: "/projects/Bistro.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/redux.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
 ];
 export const Experiences = [
- {
-  id: 1,
-  title: "Frontend Engineer Intern",
-  desc:
-   "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
-  className: "md:col-span-2",
-  thumbnail: "/exp1.svg",
- },
- {
-  id: 2,
-  title: "Mobile App Dev - JSM Tech",
-  desc:
-   "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-  className: "md:col-span-2", // change to md:col-span-2
-  thumbnail: "/exp2.svg",
- },
- {
-  id: 3,
-  title: "Freelance App Dev Project",
-  desc:
-   "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-  className: "md:col-span-2", // change to md:col-span-2
-  thumbnail: "/exp3.svg",
- },
- {
-  id: 4,
-  title: "Lead Frontend Developer",
-  desc:
-   "Developed and maintained user-facing features using modern frontend technologies.",
-  className: "md:col-span-2",
-  thumbnail: "/exp4.svg",
- },
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
 ];
 export const logos = [
- {
-  width: 220,
-  image: "/skills/firebase.png",
- },
- {
-  width: 200,
-  image: "/skills/supabase.png",
- },
- {
-  width: 150,
-  image: "/skills/nextjs.png",
- },
- { width: 140, image: "/skills/reactjs.png" },
- { width: 150, image: "/skills/strapi.png" },
- { width: 150, image: "/skills/threejs.png" },
- { width: 200, image: "/skills/appwrite.png" },
- { width: 200, image: "/skills/bootstrap.png" },
- { width: 160, image: "/skills/docker.png" },
- { width: 190, image: "/skills/react-native.png" },
- { width: 240, image: "/skills/tailwind.png" },
- { width: 180, image: "/skills/express.png" },
- { width: 180, image: "/skills/aceternity.png" },
+  {
+    width: 220,
+    image: "/skills/firebase.png",
+  },
+  {
+    width: 200,
+    image: "/skills/supabase.png",
+  },
+  {
+    width: 150,
+    image: "/skills/nextjs.png",
+  },
+  { width: 140, image: "/skills/reactjs.png" },
+  { width: 150, image: "/skills/strapi.png" },
+  { width: 150, image: "/skills/threejs.png" },
+  { width: 200, image: "/skills/appwrite.png" },
+  { width: 200, image: "/skills/bootstrap.png" },
+  { width: 160, image: "/skills/docker.png" },
+  { width: 190, image: "/skills/react-native.png" },
+  { width: 240, image: "/skills/tailwind.png" },
+  { width: 180, image: "/skills/express.png" },
+  { width: 180, image: "/skills/aceternity.png" },
+  { width: 180, image: "/skills/github.png" },
+  { width: 200, image: "/skills/shadcn.png" },
+  { width: 180, image: "/skills/redux.png" },
+  { width: 200, image: "/skills/mongodb.png" },
 ];
 
 export const socialMedia = [
- {
-  id: 1,
-  img: "/git.svg",
- },
- {
-  id: 2,
-  img: "/twit.svg",
- },
- {
-  id: 3,
-  img: "/insta.svg",
- },
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/insta.svg",
+  },
 ];
