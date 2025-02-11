3f8268e106c28da458c806169ce183e625daba94
Ammar ElShreef <162145767+AmmarElShreeif@users.noreply.github.com>
2025-02-12 01:32:40 +0200

    first commit

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -17,7 +17,7 @@ const Projects = () => {
      >
       <PinContainer
        title="/ui.aceternity.com"
-       href="https://twitter.com/mannupaaji"
+       href={item.link}
       >
        <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
         <div
