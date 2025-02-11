35df4eb4dfdbdc9ae74005102874d08c230f552e
Ammar ElShreef <162145767+AmmarElShreeif@users.noreply.github.com>
2025-02-12 01:47:37 +0200

    first commit

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -106,6 +106,7 @@ const Contact = () => {
               type="submit"
               variant="secondary"
               className="py-5 w-1/2 mx-auto text-sm font-bold"
+              onClick={handlSubmit}
             >
               Send
             </Button>
